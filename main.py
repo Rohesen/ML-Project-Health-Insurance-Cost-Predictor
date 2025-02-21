9ab5114aadc164deb126ce5b34c193f077110b7d
Rohesen Rajkamal Maurya <65121638+Rohesen@users.noreply.github.com>
2025-02-21 15:08:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import streamlit as st
 from prediction_helper import predict
 
 # Define the page layout
-st.title('Health Insurance Cost Predictor')
+st.title('Health ❤️‍🩹 Insurance Cost Predictor')
 
 categorical_options = {
     'Gender': ['Male', 'Female'],
